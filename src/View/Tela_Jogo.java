74879cc1f79f9e4654b972c66f30b01eb19fbd62
Roberto Mago de Souza <robertomagodesouza@gmail.com>
2026-07-26 22:56:54 -0300

    Add files via upload

--- src/View/Tela_Jogo.java
+++ src/View/Tela_Jogo.java
@@ -2,21 +2,26 @@ package View;
 
 import Controller.Controller_Heroi;
 import Controller.Controller_Tela;
-import Model.Item;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Tela_Jogo extends Application {
 	
@@ -30,58 +35,79 @@ public class Tela_Jogo extends Application {
 		controllerHeroi = new Controller_Heroi();
 		controllerHeroi.criarHeroi("Henry", 100, 25);
 		
-		controllerTela = new Controller_Tela(controllerHeroi.getHeroi());
+		//controllerTela = new Controller_Tela(controllerHeroi.getHeroi(), controllerHeroi);
 
 		StackPane janela = new StackPane();
 		janela.setStyle("-fx-background-color: black;");
-
+		
+		Scene scene = new Scene(janela, 800, 600);
+		
+		controllerTela = new Controller_Tela(controllerHeroi.getHeroi(), controllerHeroi, scene);
+		
 		GridPane telaC = new GridPane();
 		// Começo Tela Esquerda GridPane(0, 0)
 		
+		ColumnConstraints c1 = new ColumnConstraints();
+		c1.setPercentWidth(25);
+
+		ColumnConstraints c2 = new ColumnConstraints();
+		c2.setPercentWidth(50);
+
+		ColumnConstraints c3 = new ColumnConstraints();
+		c3.setPercentWidth(25);
+
+		telaC.getColumnConstraints().addAll(c1, c2, c3);
+		
+		
 		
 		telaC.add(controllerTela.getStatus(), 0, 0);
-		
+		//telaC.setTop(controllerTela.getStatus());
 		// Fim da Tela Esquerda GridPane(0, 0)
 		
 		// Começo da Tela Esquerda GridPane(0, 1)
 		
-		VBox telaEsquerdaBaixo = new VBox(10);
+		telaC.add(controllerTela.getInventario(), 0, 1);
+		//telaC.setLeft(controllerTela.getInventario());
+		// Fim da Tela Esquerda GridPane(0, 1)
 		
-		telaEsquerdaBaixo.setAlignment(Pos.CENTER);
+		telaC.add(controllerTela.getButton(), 2, 1);
+		//telaC.setBottom(controllerTela.getButton());
+		telaC.add(controllerTela.getTexto(), 2, 0);
 		
-		TableView<Item> inventario = new TableView<>();
-		//Continuar
-		Button usar = new Button("Usar");
+		//telaC.setRight(controllerTela.getTexto());
+		
+		telaC.add(controllerTela.getImagem(), 1, 0, 1, 2);
+		
+		/*ImageView imagem = new ImageView(getClass().getResource("/Imagens/Pagina_1.png").toExternalForm());
+		
+		//imagem.setFitHeight(400);
+		//imagem.setFitWidth(200);
+		
+		//imagem.setPreserveRatio(true);
+		 
+		Pane teste = new Pane(imagem);
+		
+		imagem.fitHeightProperty().bind(teste.heightProperty());
+		imagem.fitWidthProperty().bind(teste.widthProperty());
+		
+		telaC.add(teste, 1, 0, 1, 2);*/
+		//telaC.setCenter(imagem);
+		
+		//telaC.setAlignment(Pos.CENTER);
 		
 		
+		janela.getChildren().addAll(telaC, controllerTela.getDadoHeroi());
 		
-		
-		telaEsquerdaBaixo.getChildren().addAll(usar, inventario);
-		
-		TableColumn<Item, String> colNome = new TableColumn<>("Nome");
-		colNome.setCellValueFactory(cellData -> cellData.getValue().nomeProperty());
-		
-		TableColumn<Item, Integer> colUnidade = new TableColumn<>("Quant.");
-		colUnidade.setCellValueFactory(cellData -> cellData.getValue().unidadeProperty().asObject());
-		
-		inventario.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
-		
-		inventario.getColumns().addAll(colNome, colUnidade);
-		
-		inventario.setItems(controllerHeroi.getInventario());
-		
-		
-		telaC.add(telaEsquerdaBaixo, 0, 1);
-		
-		janela.getChildren().add(telaC);
+		//controllerTela.aumentarTela(janela);
 
-		Scene scene = new Scene(janela, 600, 600);
+		//Scene scene = new Scene(janela, 800, 600);
 
 		stage.setScene(scene);
 
 		stage.setTitle("Teste");
 
 		stage.show();
+		
 
 	}
 
